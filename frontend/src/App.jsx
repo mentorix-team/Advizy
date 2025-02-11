70b97f5e913dc22590189e257bae3b21d63b05bd
ProcessDesigner <gurdev191004@gmail.com>
2025-02-11 19:18:09 +0530

    Deleted expert form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,13 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./Home";
 import ExpertDashboardRoutes from "./routes/ExpertDashboardRoutes";
 import UserDashboardRoutes from "./routes/UserDashboardRoutes";
-import RegistrationRoutes from "./routes/RegistrationRoutes";
-import Explore from "./Explore";
 import ExpertDetailPage from "./components/Expert/ExpertDetailProfile/ExpertDetailPage";
 import Scheduling from "./components/Dashboard/User/Scheduling/Scheduling";
 import OrderSummary from "./components/Dashboard/User/OrderSummary";
-import ExpertProfile from "./components/Dashboard/User/Scheduling/components/ExpertProfile";
-import ServiceForm from "./components/Dashboard/Expert/ServiceAndPricing/ServiceModal/ServiceForm";
 import BookingConfirmation from "./components/Dashboard/Expert/Meetings/BookingConfirmation";
 import Meeting from "./components/Meeting/Meeting";
 import HomePage from "./components/Home/pages/HomePage";
 import BecomeExpertPage from "./components/Home/pages/BecomeExpertPage";
-import ExpertList from "./components/ExpertList/src/components/ExpertList";
-import ExpertEducation from "./components/ExpertForms/ExpertEducation";
 import Homees from "./components/Explore/Homees";
 import ReScheduling from "./components/Dashboard/User/Scheduling/ReScheduling";
 import ProtectedRoute from "./Protected/ProtectedRoute";
@@ -23,7 +15,6 @@ import GoogleAuthSuccess from "./components/Auth/GoogleAuthSuccess";
 import Error404 from "./Protected/Error404";
 
 
-// import ServiceModal from "./components/Dashboard/Expert/ServiceAndPricing/components/ServiceModal";
 
 const App = () => {
 
@@ -32,8 +23,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/become-expert" element={<BecomeExpertPage />} />
-        <Route path="/services" element={<ServiceForm />} />
-        {/* <Route path="experts" element={<ExpertList/>}/> */}
         <Route path="/explore" element={<Homees />} />
         <Route path="/expert/:id" element={<ExpertDetailPage />} />
         <Route path="/expert/scheduling/:serviceId" element={<Scheduling />} />
